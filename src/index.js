637164777f7cce55f4b574d655cdc77634473d30
Oscar Julian Gil <oscarjuligil@gmail.com>
2022-11-16 21:12:43 -0500

    cambio de politica cors

--- src/index.js
+++ src/index.js
@@ -12,12 +12,9 @@ const ecommerceRouter = require('./routes/ecommerceController');
 require('dotenv').config();
 
 const app = express();
-app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-//app.use(cors());
+
+app.use(cors());
+app.options('*', cors());
 //Configuramos express como json data
 app.use(express.json({limit: '50mb'}));
 //Ruta inicial
